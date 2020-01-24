714cf75b9143971813dc1e5c6c1b460a1255fa7a
Tess Huang <tesshuangxj@yahoo.com>
2020-01-24 11:06:00 -0800

    add materializecss and rollupcssplugin and routing packages to the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <script>
 	export let name;
+
+	import "../node_modules/materialize-css/dist/css/materialize.min.css";
+	import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
 </script>
 
 <main>
@@ -8,23 +12,5 @@
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
