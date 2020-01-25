f9a5bf930acda30375b7b1877f412f6465038cfd
Tess Huang <tesshuangxj@yahoo.com>
2020-01-24 16:52:50 -0800

    add layout comp and page comps

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,27 @@
 <script>
-	export let name;
+	// export let name;
 
 	import "../node_modules/materialize-css/dist/css/materialize.min.css";
 	import "../node_modules/materialize-css/dist/js/materialize.min.js";
+	import { Router, Route } from "svelte-routing";
 
+	import NavBar from './layout/NavBar.svelte'; 
+	
+	import Home from "./pages/Home.svelte";
+	import About from './pages/About.svelte';
+	import Contact from "./pages/Contact.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="container">
+	<Router>
+		<NavBar />
+		<div>
+			<Route path="contact" component="{Contact}" />
+			<Route path="about" component="{About}" />
+			<Route path="/"><Home /></Route>
+	</div>
+	</Router>
+</div>
 
 <style>
 
---
+++ src/layout/NavBar.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<nav>
+  <div class="nav-wrapper">
+    <Link to="/" class="brand-logo">Logo</Link>
+    <ul id="nav-mobile" class="right">
+      <li><Link to="about">About</Link></li>
+      <li><Link to="contact">Contact</Link></li>
+    </ul>
+  </div>
+</nav>
