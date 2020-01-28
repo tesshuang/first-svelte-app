9ddacd84bd94b12c933dd1353c6dbe9acc965cda
Tess Huang <tesshuangxj@yahoo.com>
2020-01-28 11:40:19 -0800

    Add Image component

---
+++ src/components/Image.svelte
@@ -0,0 +1,11 @@
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.materialboxed');
+    let instances = M.Materialbox.init(elems, options);
+  });
+</script>
+
+<div>
+  <img src="https://source.unsplash.com/daily" alt="daily Image" data-caption="Daily image from unsplash" width="350" class="materialboxed">
+</div>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,12 @@
-	<script>
-			export let name;
-	</script>
+<script>
+		export let name;
+		
+		import Image from '../components/Image.svelte';
+
+</script>
+<div class="container">
 	<h1>Hello {name}</h1>
+	<Image />
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</div>
+
