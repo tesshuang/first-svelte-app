d03f04d3293e792211fdef6af4afead917e0fa32
Tess Huang <tesshuangxj@yahoo.com>
2020-01-29 16:59:56 -0800

    Add modal component to app

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -3,9 +3,10 @@
   document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.materialboxed');
     let instances = M.Materialbox.init(elems, options);
+    var instance = M.Materialbox.getInstance(elem);
   });
 </script>
 
 <div>
-  <img src="https://source.unsplash.com/daily" alt="daily Image" data-caption="Daily image from unsplash" width="350" class="materialboxed">
+  <img src="https://source.unsplash.com/daily" alt="daily Image" data-caption="Daily image from unsplash" width="350" class="materialboxed responsive-img">
 </div>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,12 +1,15 @@
 <script>
-		export let name;
-		
-		import Image from '../components/Image.svelte';
+	export let name;
+	
+	import Image from '../components/Image.svelte';
+	import Modal from '../components/Modal.svelte';
 
 </script>
 <div class="container">
 	<h1>Hello {name}</h1>
 	<Image />
+	<br/>
+	<Modal />
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </div>
 
